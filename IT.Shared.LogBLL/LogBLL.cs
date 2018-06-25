959b7b2767b4feef85b5555ead264ba26cf186ce
puvvadivk4 <38309330+puvvadivk4@users.noreply.github.com>
2018-06-25 13:19:42 +0530

    Add files via upload

---
+++ IT.Shared.LogBLL/LogBLL.cs
@@ -0,0 +1,27 @@
+﻿using IT.Shared.LogEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IT.Shared.LogDAL;
+
+namespace IT.Shared.LogBLL
+{
+    public class LogBLL
+    {
+        LogDAL.LogDAL dal;
+        public List<Error> ErrorBLL(Error error, String environment)
+        {
+
+            dal = new LogDAL.LogDAL();
+            return dal.ErrorDAL(error, environment);
+
+        }
+        public List<Message> MessageBLL(Message message, String environment)
+        {
+            dal = new LogDAL.LogDAL();
+            return  dal.MessagDAL(message, environment);
+        }
+    }
+}
